396de779e3c08969f653697faeaffc88d1889fe0
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-16 10:54:03 +0530

    getting variable from cmd line

---
+++ 03-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "hi this Jayendra, i am getting variable from cmd line."
+
+person1=$1
+person2=$2
+echo "$person1: hi ra!, cat"
+echo "$person2: meow!"
+echo "$person1: how it's going "
+echo "$person2: meow,meoww"
+echo "$person1: dandam ra dutha"
