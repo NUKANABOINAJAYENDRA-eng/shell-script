2d6213f05847882ec1d4e2110b0537016dabe683
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-16 10:39:09 +0530

    showing DATE

--- 02-DATE.sh
+++ 02-DATE.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#shell script run the command inside $() and stores the output into variable called DATe
+#shell script run the command inside $() and stores the output into variable called DATE
 DATE=$(date)
 echo "this line is exicuted on:$DATE"
