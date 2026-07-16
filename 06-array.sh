d118a5b5814f12811ca95afbf5da51b69ccaa0eb
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-16 11:47:23 +0530

    practicing array

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#arry can hold the multiple values in it
+
+PERSONS=("JAYENDRA","RAMESH", "SURESH")
+#ARRAY starts the index value from 0
+
+echo "First person is: ${PERSONS[0]}"
+#to get the required person from array
