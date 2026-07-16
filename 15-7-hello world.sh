449edfe48c220fe3a074afef12127f34131737d8
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-16 09:02:38 +0530

    updated hello world cmd

--- 15-7-hello world.sh
+++ 15-7-hello world.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 
-
 echo "hi this Jayendra, i am learning devop+AWS from the past 3 weeks."
+echo "jay: hi ra!, cat"
+echo "cat: meow!"
+echo "jay:"how it's going "
+echo "cat:"meow,meoww"
+echo "jay: dandam ra dutha"
