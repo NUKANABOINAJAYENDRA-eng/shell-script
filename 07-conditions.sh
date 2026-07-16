75026c3853eeee87914f7ab2dfa5da86ac088bb8
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-16 16:51:10 +0530

    conditing in shell-script

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,9 +3,9 @@
 #checking the number is gerater than 7 or not
 Number=$1
 
-if [$Number -gt 7]
+if [ $Number -gt 7 ]
 then
     echo "$Number is greater than 7"
 else
-    echo "$number is less than 7"
+    echo "$Number is less than 7"
 fi
