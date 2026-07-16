40e5ff158d965f460c710b39956130a8f8ac92fa
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-16 11:03:46 +0530

    addtion of two numbers

--- 03-variable.sh
+++ 03-variable.sh
@@ -3,8 +3,8 @@
 
 echo "hi this Jayendra, i am getting variable from cmd line."
 
-person1=$1
-person2=$2
+person1=$1 # this is first value from command line
+person2=$2 # this is second value from command line
 echo "$person1: hi ra!, cat"
 echo "$person2: meow!"
 echo "$person1: how it's going "
