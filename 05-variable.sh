7827295ee6ce770baf6a4b08b9cabdd238e69f7f
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-16 11:31:30 +0530

    initial commit

---
+++ 05-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username:"
+
+read -s username # this is the variable where the value of the user is entered
+
+echo " username entered is :$username"
+
+echo "please enter your password"
+
+read -s password 
+
+echo " password entered is: $password"
