43349ad867df5131025b7a5ae45ee87e2204d99a
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-17 09:48:28 +0530

    special variables

--- 09-functions.sh
+++ 09-functions.sh
@@ -10,15 +10,19 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 # this function shoul validate the previous cmd and inform user it is success or a failure.
 VALIDATE(){
     #1 --> it will recive the argument-1
     if [ $1 -ne 0 ]
     then
-            echo "$2...FAILURE"
+            echo "$2...$R FAILURE $N"
             exit 1
     else
-        echo "$2...success"
+        echo "$2...$G success $N"
     fi
 }
 
@@ -36,9 +40,9 @@ fi
 #it is our responsibility to check installation is success or not
 yum install mysql -y &>>$LOGFILE
 
-VALIDATE $? "Installing mysql"
+VALIDATE $? "$G Installing mysql $N"
 
 #installing postfix
 yum install postfix -y &>>$LOGFILE
 
-VALIDATE $? "installing postfix"
+VALIDATE $? "$G installing postfix $N"
