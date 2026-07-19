32c9ae858093e82ef8d8a4cb43765a98f4f7342e
JAYENDRA NUKANABOINA <nukanaboinajayrndra5@gmail.com>
2026-07-19 10:53:01 +0530

    deleting old files of past 2 weeks

--- 14-delete-old-logfiles.sh
+++ 14-delete-old-logfiles.sh
@@ -10,3 +10,11 @@ LOGFILES=/tmp/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 echo "$FILES_TO_DELETE"
+
+
+while read line 
+do 
+    #we write our statements here
+        echo "Deleting $line" $> $LOGFILE
+        rm -rf $line
+done <<< $FILES_TO_DELETE
